54d4a33bdae5875cf5cb7bace6054eceaa0e8d39
Pankaj-Shinde-19 <pankajharishinde19@gmail.com>
2023-07-13 16:46:55 +0530

    Changes Done in Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-  
+    class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
+    public void main(String[] args) {
+            // Task 3
+            B obj = new B();
+
+            // Task 5
+            String result = obj.meth();
+            System.out.println(result);
+    }
 }
