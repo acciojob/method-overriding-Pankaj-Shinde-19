67844b33835a366fbb8ce8e93961cb3917e85e38
Pankaj-Shinde-19 <pankajharishinde19@gmail.com>
2023-07-13 16:56:17 +0530

    Code Changes Done - 2nd Time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,19 @@
 package com.driver;
 
 public class Main {
-    class A {
-        public String meth() {
+    public static class A {
+        public static String meth() {
             return "Invoking method from class A";
         }
     }
 
-    class B extends A {
-        @Override
-        public String meth() {
+    public static class B extends A {
+        public static String meth() {
             return "Method is overridden in Extended class B";
         }
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
             // Task 3
             B obj = new B();
 
